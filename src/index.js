905037a25df84cd3436be247e2b119fa947fc112
Mohd Faizan <gitfaizan0496@gmail.com>
2023-04-18 01:02:40 +0530

    setting up the index file and add provider and store ,redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { configureStore } from 'redux';
+import contactReducer from './redux/reducers/contactReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+
+const store = configureStore(contactReducer, composeWithDevTools());
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+
+    
   </React.StrictMode>
 );
 
